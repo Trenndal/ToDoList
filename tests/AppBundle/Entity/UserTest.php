8017131d00c10b0cd0b3a664fbdb9ba06c0389e0
Trenndal <reymond.vincent@outlook.fr>
2018-03-15 09:25:54 +0100

    Add unitary tests

---
+++ tests/AppBundle/Entity/UserTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\AppBundle\Entity;
+
+use AppBundle\Entity\User;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+
+    public function testUsername()
+    {
+        $user = new User();
+        
+        $user->setUsername("Username");
+        $this->assertSame("Username", $user->getUsername());
+    }
+
+    public function testPassword()
+    {
+        $user = new User();
+        
+        $user->setPassword("Password");
+        $this->assertSame("Password", $user->getPassword());
+    }
+
+    public function testEmail()
+    {
+        $user = new User();
+        
+        $user->setEmail("Email");
+        $this->assertSame("Email", $user->getEmail());
+    }
+
+    public function testRoles()
+    {
+        $user = new User();
+        
+        $Roles = array('ROLE_USER');
+        //$user->setRoles($Roles);
+        $this->assertSame($Roles, $user->getRoles());
+    }
+}
